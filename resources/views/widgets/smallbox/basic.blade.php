f0f603dec476fbd8a68f5ec1911ea8f39be5835d
JIAO Jie <thomasjiao@vip.qq.com>
2016-07-04 16:49:23 +0800

    small box

--- resources/views/widgets/smallbox/basic.blade.php
+++ resources/views/widgets/smallbox/basic.blade.php
@@ -1,7 +1,7 @@
 <!-- small box -->
 <div class="small-box bg-{{$color or "yellow"}}">
     <div class="inner">
-        <h3>{{ $data or "Null" }}</h3>
+        <h3>{!! $data or "Null" !!}</h3>
 
         <p>{{$desc or "An error happened."}}</p>
     </div>
