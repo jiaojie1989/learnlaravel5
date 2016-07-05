6bfc5ba3da6039b28f7dceed9af3f81667a068ab
JIAO Jie <thomasjiao@vip.qq.com>
2016-07-05 18:53:39 +0800

    Test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,11 +10,25 @@ class TestController extends Controller {
 
     public function test() {
 //        return view("welcome");
-        $grid = \DataGrid::source(new PayOrder());
-        $grid->add('id', 'id', true)->style("width:100px"); //relation.fieldname 
-        $grid->add('order_id','Body');;
-        $grid->paginate(10);
-        return view('test', compact('grid'));
+        $filter = \DataFilter::source(new PayOrder());
+        $filter->add('order_id', 'Order Id', 'text');
+        $filter->add('pay_id', 'Pay Id', 'text');
+        $filter->submit('search');
+        $filter->reset('reset');
+        $filter->build();
+
+        $grid = \DataGrid::source($filter);
+        $grid->add('id', '#', true); //relation.fieldname 
+        $grid->add('order_id', 'Order Id');
+        $grid->add('sid', 'Sid');
+        $grid->add('total_payments', 'Payment');
+        $grid->add('pay_id', "Pay Id");
+        $grid->paginate(20);
+        $grid->attributes(["class" => "table table-bordered table-striped dataTable"]);
+        $grid->edit('/rapyd-demo/edit', 'Edit', 'show|modify');
+        $grid->link('/rapyd-demo/edit', "New Article", "TR");
+        \Kint::dump($grid);
+        return view('test', compact('grid', 'filter'));
     }
 
 }
