50a6104220debb4c007de99f880679182aa143b0
JIAO Jie <thomasjiao@vip.qq.com>
2016-07-05 18:53:51 +0800

    Test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,14 +1,24 @@
-@extends('rapyd::demo.demo')
+@extends("layouts.dashboard")
 
-@section('title','DataGrid')
-
-@section('body')
-
-    <h1>DataGrid</h1>
-    <p>
-
-        {!! $grid !!}
-        
-        
-    </p>
+@section("content")
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    @include("contents.title", ["home" => "Home", "partname" => "Dashboard"])
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Table</h3>
+                    </div>
+                    <div class="box-body">
+                        {!! $filter !!}
+                        {!! $grid !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<!-- /.content-wrapper -->
 @stop
