d040c4e825617288297196c464fcdafde734534c
JIAO Jie <thomasjiao@vip.qq.com>
2016-07-14 18:57:25 +0800

    Route Test

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class DemoController extends Controller {
+
+    public function getNudecomponent() {
+        
+    }
+
+    public function putNudecomponent() {
+        
+    }
+
+    public function getIndex() {
+        
+    }
+
+}
